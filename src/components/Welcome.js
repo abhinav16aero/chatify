8143e9df85219ec27a2a6e0dcfc0021ddbe67790
Abhinav <114936870+abhinav16aero@users.noreply.github.com>
2023-05-19 19:35:32 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -14,14 +14,14 @@ const Welcome = () => {
       <h2>Welcome to the Chat.</h2>
 //       <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
       <p>Sign in with Google to chat and enable all cookies for the smooth experience. Also, wait for 10 sec for the chatroom to load.</p>
-//       <button className="sign-in">
-//         <img
-//           onClick={googleSignIn}
-//           src={GoogleSignin}
-//           alt="sign in with google"
-//           type="button"
-//         />
-//       </button>
+      <button className="sign-in">
+        <img
+          onClick={googleSignIn}
+          src={GoogleSignin}
+          alt="sign in with google"
+          type="button"
+        />
+      </button>
     </main>
   );
 };
